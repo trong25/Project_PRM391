3c11f52f5fdecdae32cfec50e97e31ae32450bc0
trong25 <he181249nguyenvantrong@gmail.com>
2026-06-08 22:12:16 +0700

    Add files via upload

---
+++ backend/src/main/java/genZ/PRM391GenZ/Prm391GenZApplication.java
@@ -0,0 +1,13 @@
+package genZ.PRM391GenZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prm391GenZApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prm391GenZApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/genZ/PRM391GenZ/Prm391GenZApplicationTests.java
@@ -0,0 +1,13 @@
+package genZ.PRM391GenZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prm391GenZApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
