8b58486ecc0d9fde14dff4b2e5bf3be4b967bd00
huyboom22 <huyboom0202@gmail.com>
2026-07-19 21:21:42 +0700

    huy

--- backend/src/main/java/genZ/PRM391GenZ/Prm391GenZApplication.java
+++ backend/src/main/java/genZ/PRM391GenZ/Prm391GenZApplication.java
@@ -7,9 +7,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @SpringBootApplication
 public class Prm391GenZApplication {
 	public static void main(String[] args) {
-		//Chay lenh de hash password 123456, thay hash o phan 'run' vao phan password admin truoc khi Insert
-		//BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		//System.out.println(encoder.encode("123456"));
+//		Chay lenh de hash password 123456, thay hash o phan 'run' vao phan password admin truoc khi Insert
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		System.out.println(encoder.encode("123456"));
 		SpringApplication.run(Prm391GenZApplication.class, args);
 	}
 }
